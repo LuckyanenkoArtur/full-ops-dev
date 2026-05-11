e2a0cb60114753928ab5905bc73a3400e37c650e
Arthur Lukyanenko <lukyanenko@gmail.com>
2026-05-11 13:03:23 -0700

    Add styling and add the icons

--- src/app/components/home-section/home-section.tsx
+++ src/app/components/home-section/home-section.tsx
@@ -1,15 +1,19 @@
 import { useTranslations } from "next-intl";
 import styles from "./home-section.module.scss";
-
-// TODO: add th image and add the stylign better
+import { BoostyIcon } from "./icons/boosty";
+import { BastyonIcon } from "./icons/bastyon";
+import { GitHubIcon } from "./icons/github";
+import { LinkedInIcon } from "./icons/linkedin";
+import { TelegramIcon } from "./icons/telegram";
+import { WhatsAppIcon } from "./icons/whatsapp";
+import { MaxIcon } from "./icons/max";
 
 const HomeSection = () => {
   const t = useTranslations("home-section");
 
   return (
     <section className={styles["home-section-container"]}>
-      {/* CONTENT */}
-      <div>
+      <div className={styles["content-side"]}>
         <div className={styles["greetings-text"]}>{t("greetings-text")}</div>
 
         <div className={styles["title"]}>
@@ -25,9 +29,21 @@ const HomeSection = () => {
         <div className={styles["paragraph-text"]}>{t("paragraph-text")}</div>
 
         <button className={styles["about-button"]}>{t("button.text")}</button>
+        <div>
+          <MaxIcon />
+          <WhatsAppIcon />
+          <TelegramIcon />
+          <LinkedInIcon />
+          <BoostyIcon />
+          <BastyonIcon />
+          <GitHubIcon />
+        </div>
       </div>
-      {/* IMAGE */}
-      <div></div>
+      <div className={styles["hero-side"]}>
+        <div className={styles["hero-background"]}>
+          <img src={"/assets/images/hero-me-1.png"} alt="hero-image" />
+        </div>
+      </div>
     </section>
   );
 };
