a70adb0f8682f5cbc08b7dd82582ad363f26ed9f
Arthur Lukyanenko <lukyanena@gmail.com>
2026-05-11 13:12:35 +0300

    Convert the px to rem in Header component

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -3,9 +3,9 @@ import { useTranslations } from "next-intl";
 import ThemeToggle from "@components/header/theme-toggle/theme-toggle";
 import LanguageSelection from "@/app/components/header/language-selection/language-selection";
 import Logo from "@components/header/logo/logo";
+import Navigation from "./navigation/navigatation";
 
 import styles from "./header.module.scss";
-import Navigation from "./navigation/navigatation";
 
 const Header = () => {
   const t = useTranslations("HomePage");
