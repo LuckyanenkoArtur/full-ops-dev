9e647b4b0a1dfa8b69c8eb4e067fa2e6bfd75886
Arthur Lukyanenko <lukyanenko@gmail.com>
2026-05-11 20:32:32 -0700

    Home-Section compoentn

--- src/app/components/home-section/home-section.tsx
+++ src/app/components/home-section/home-section.tsx
@@ -7,42 +7,53 @@ import { LinkedInIcon } from "./icons/linkedin";
 import { TelegramIcon } from "./icons/telegram";
 import { WhatsAppIcon } from "./icons/whatsapp";
 import { MaxIcon } from "./icons/max";
+import { ScrollerIcon } from "./icons/scroller";
 
 const HomeSection = () => {
   const t = useTranslations("home-section");
 
   return (
     <section className={styles["home-section-container"]}>
-      <div className={styles["content-side"]}>
-        <div className={styles["greetings-text"]}>{t("greetings-text")}</div>
+      <div className={styles["home-section-container-flex-wrapper"]}>
+        <div className={styles["content-side"]}>
+          <div className={styles["greetings-text"]}>{t("greetings-text")}</div>
 
-        <div className={styles["title"]}>
-          {t.rich("title", {
-            span: (chunks) => (
-              <span className={styles["highlight"]}>{chunks}</span>
-            ),
-          })}
+          <div className={styles["title"]}>
+            {t.rich("title", {
+              span: (chunks) => (
+                <span className={styles["highlight"]}>{chunks}</span>
+              ),
+            })}
+          </div>
+
+          <div className={styles["position-text"]}>{t("position-text")}</div>
+
+          <div className={styles["paragraph-text"]}>{t("paragraph-text")}</div>
+
+          <div className={styles["social-button-container"]}>
+            <button className={styles["about-button"]}>
+              {t("button.text")}
+            </button>
+            <div className={styles["social-icons"]}>
+              <MaxIcon />
+              <WhatsAppIcon />
+              <TelegramIcon />
+              <LinkedInIcon />
+              <BoostyIcon />
+              <BastyonIcon />
+              <GitHubIcon />
+            </div>
+          </div>
         </div>
-
-        <div className={styles["position-text"]}>{t("position-text")}</div>
-
-        <div className={styles["paragraph-text"]}>{t("paragraph-text")}</div>
-
-        <button className={styles["about-button"]}>{t("button.text")}</button>
-        <div>
-          <MaxIcon />
-          <WhatsAppIcon />
-          <TelegramIcon />
-          <LinkedInIcon />
-          <BoostyIcon />
-          <BastyonIcon />
-          <GitHubIcon />
+        <div className={styles["hero-side"]}>
+          <div className={styles["hero-background"]}>
+            <img src={"/assets/images/hero-me-1.png"} alt="hero-image" />
+          </div>
         </div>
       </div>
-      <div className={styles["hero-side"]}>
-        <div className={styles["hero-background"]}>
-          <img src={"/assets/images/hero-me-1.png"} alt="hero-image" />
-        </div>
+
+      <div className={styles["home-section-container-scroller-container"]}>
+        <ScrollerIcon />
       </div>
     </section>
   );
